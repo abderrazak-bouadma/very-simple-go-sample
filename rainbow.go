597a08c9266231e4dc054e4c80b4495af24b05b8
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2017-08-31 01:16:42 +0200

    restored deleted go program :ghost:

---
+++ rainbow.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func handleRootContext(w http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(w, "<!doctype html>"+
+		"<html>"+
+		"<head>"+
+		"<meta charset=\"utf-8\">"+
+		"<title>A static page</title>"+
+		"<style>"+
+		"body {background-color: purple}"+
+		"</style>"+
+		"</head>"+
+		"<body>"+
+		"<h1>Hello from a static page</h1>"+
+		"</body>"+
+		"</html>")
+}
+
+func handleHealthCheck(w http.ResponseWriter, request *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, `{"alive": true}`)
+}
+
+func main() {
+	http.HandleFunc("/", handleRootContext)
+	http.HandleFunc("/health", handleHealthCheck)
+	log.Println("Listening ....")
+	http.ListenAndServe(":3000", nil)
+}
