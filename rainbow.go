a5b85daf95a3218a812e1d18e8208804525a6341
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2017-09-10 22:32:11 +0200

    loging the port

--- rainbow.go
+++ rainbow.go
@@ -9,6 +9,6 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
 
-	log.Println("Listening...")
+	log.Println("Listening on port 3000...")
 	http.ListenAndServe(":3000", nil)
 }
